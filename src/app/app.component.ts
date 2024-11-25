3cb068f18b9503386f9a4dae3c449806faaef14a
Murilo Cruz <murilomecr@gmail.com>
2024-11-25 12:52:14 -0300

    adding mise.toml and nodejs version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: '<router-outlet>/router-outlet>',
+  template: '<router-outlet></router-outlet>',
 })
 export class AppComponent { }
